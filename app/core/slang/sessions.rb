73e5a1a8d988905b094ebd4d102fb64e36ce0127
alejolopez91 <allejo392@gmail.com>
2022-02-04 00:35:42 -0500

    rework sessions to match response format

--- app/core/slang/sessions.rb
+++ app/core/slang/sessions.rb
@@ -1,56 +1,76 @@
 module Slang
   class Sessions
-    attr_reader :provider
+    attr_reader :provider, :response
 
-    def initialize(provider: Slang::Activities.new)
+    def initialize(provider: Slang::Activities.new, api_client: Slang::Api.new)
       @provider = provider
     end
 
     def sort_sessions
       raw_data = provider.get_activities
-      sorted_data = process_data(raw_data["activities"])
+      @response = process_data(raw_data["activities"])
+    end
+
+    def post_results
+      api_client.post(body: response)
     end
 
     private
 
     def process_data(data)
-      grouped_data = group_by_id(data)
-      ordered_data = order_in_sessions_data(grouped_data)
-
-      { "user_sessions": ordered_data }
-    end
-
-    def group_by_id(data)
-      dict = {}
-      data.each do |element|
-        data_obj = dict[element["user_id"]] || first_element(element["user_id"])
-        data_obj["answered_at"].push(Time.parse(element["answered_at"]))
-        data_obj["first_seen_at"].push(Time.parse(  element["first_seen_at"]))
-        data_obj["activity_ids"].push(element["id"])
-        dict[element["user_id"]] = data_obj
-      end
-
-      dict
-    end
-
-    def order_data(data)
-      data.map do |id, element|
-        first_date = element["first_seen_at"].min
-        last_date = element["answered_at"].max
-        element["first_seen_at"] = first_date
-        element["answered_at"] = last_date
-        element["duration_seconds"] = (first_date - last_date) / 1
-        
-        element
+      grouped_data = group_by_user_id(data)
+      sorted_dates = grouped_data.map do |id, activities|
+        sorted = sort_date(activities)
+        { "#{id}": divide_sessions(sorted) }.with_indifferent_access
       end
     end
 
-    def first_element(user_id)
+    def group_by_user_id(data)
+      data.group_by{ |element| element["user_id"].itself }
+    end
+
+    def sort_date(activities)
+      activities.each { |activity| activity["first_seen_at"] = Time.parse(activity["first_seen_at"]) }
+      activities.each { |activity| activity["answered_at"] = Time.parse(activity["answered_at"]) }
+    end
+
+    def divide_sessions(activities)
+      list = []
+      activities.each do |activity|
+        if list.blank?
+          list << new_first_element(activity)
+          next 
+        end
+
+        session_obj = list.last
+        if minutes_between_activities(session_obj, activity) < 5
+          session_obj["ended_at"] = activity["answered_at"]
+          session_obj["activity_ids"] << activity["id"]
+          session_obj["duration_seconds"] = seconds_between_dates(session_obj)
+          list[-1] = session_obj
+        else
+          list.last["duration_seconds"] = seconds_between_dates(session_obj)
+          list.push(new_first_element(activity))
+        end
+      end
+
+      list
+    end
+
+    def minutes_between_activities(session_obj, last_activity)
+      (last_activity["first_seen_at"] - session_obj["ended_at"]) / 1.minute
+    end
+
+    def seconds_between_dates(session_obj)
+      (session_obj["ended_at"] - session_obj["started_at"])
+    end
+
+    def new_first_element(activity)
       {  
-        "answered_at": [],
-        "first_seen_at": [],
-        "activity_ids": [],
-        "duration_seconds": ""
+        "ended_at": activity["answered_at"],
+        "started_at": activity["first_seen_at"],
+        "activity_ids": [activity["id"]],
+        "duration_seconds": activity["answered_at"] - activity["first_seen_at"]
       }.with_indifferent_access
     end
   end
